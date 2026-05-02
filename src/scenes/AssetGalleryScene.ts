bd54f87f3624f7c4090bb3e178239eb5e8ccd06f
Jared O <flamableconcrete@gmail.com>
2026-05-02 13:29:25 -0500

    feat: AssetGalleryScene skeleton with all texture generators wired in create()

--- src/scenes/AssetGalleryScene.ts
+++ src/scenes/AssetGalleryScene.ts
@@ -13,7 +13,7 @@ import { ItemData, SpellData } from '../types'
 
 type CategoryId = 'items' | 'companions' | 'enemies' | 'backgrounds' | 'spells'
 
-interface DisplayEntry {
+export interface DisplayEntry {
   key: string
   name: string
   group: string
@@ -22,16 +22,16 @@ interface DisplayEntry {
 }
 
 export class AssetGalleryScene extends Phaser.Scene {
-  private _hubContainer!: Phaser.GameObjects.Container
-  private _categoryContainer!: Phaser.GameObjects.Container
-  private _modalContainer: Phaser.GameObjects.Container | null = null
-  private _dimRect: Phaser.GameObjects.Rectangle | null = null
-  private _activeBgObjects: Phaser.GameObjects.GameObject[] = []
+  hubContainer!: Phaser.GameObjects.Container
+  categoryContainer!: Phaser.GameObjects.Container
+  modalContainer: Phaser.GameObjects.Container | null = null
+  dimRect: Phaser.GameObjects.Rectangle | null = null
+  activeBgObjects: Phaser.GameObjects.GameObject[] = []
 
-  private _activeCategory: CategoryId | null = null
-  private _activeFilter = 'All'
-  private _filteredList: DisplayEntry[] = []
-  private _allEntries: DisplayEntry[] = []
+  activeCategory: CategoryId | null = null
+  activeFilter = 'All'
+  filteredList: DisplayEntry[] = []
+  allEntries: DisplayEntry[] = []
 
   constructor() { super('AssetGallery') }
 
@@ -41,55 +41,25 @@ export class AssetGalleryScene extends Phaser.Scene {
     this.add.rectangle(width / 2, height / 2, width, height, 0x1a1a2e)
 
     // Generate all textures upfront — each generator guards against duplicates internally
-    generateGoblinWhackerTextures(this)   // 'goblin', 'ogre', 'hero', 'forest_bg', etc.
-    generateSkeletonSwarmTextures(this)   // 'ss_skeleton', 'ss_sky', 'ss_ruins', etc.
-    generateDungeonTrapTextures(this)     // 'dungeon_bg', 'trap_idle', etc.
+    generateGoblinWhackerTextures(this)     // 'goblin', 'ogre', 'hero', 'forest_bg', etc.
+    generateSkeletonSwarmTextures(this)     // 'ss_skeleton', 'ss_sky', 'ss_ruins', etc.
+    generateDungeonTrapTextures(this)       // 'dungeon_bg', 'trap_idle', etc.
     generateDungeonPlatformerTextures(this) // 'obstacle_pit', 'platform_floor', etc.
-    generateGenericBossTextures(this)     // 'generic_boss'
-    generateNessaTextures(this)           // 'nessa_boss'
-    generateCrazedCookTextures(this)      // 'cook_ladle', 'kitchen_bg', etc.
-    generateAllCompanionTextures(this)    // 'mouse_guard_scout', 'goblin' (pet), etc.
-    generateAllItemTextures(this)         // all 34 item textures
+    generateGenericBossTextures(this)       // 'generic_boss'
+    generateNessaTextures(this)             // 'nessa_boss'
+    generateCrazedCookTextures(this)        // 'cook_ladle', 'kitchen_bg', etc.
+    generateAllCompanionTextures(this)      // 'mouse_guard_scout', 'goblin' (pet), etc.
+    generateAllItemTextures(this)           // all 34 item textures
 
-    this._hubContainer = this.add.container(0, 0).setDepth(10)
-    this._categoryContainer = this.add.container(0, 0).setDepth(10).setVisible(false)
-    void this._hubContainer
-    void this._categoryContainer
-    void this._modalContainer
-    void this._dimRect
-    void this._activeBgObjects
-    void this._activeCategory
-    void this._activeFilter
-    void this._filteredList
-    void this._allEntries
+    this.hubContainer = this.add.container(0, 0).setDepth(10)
+    this.categoryContainer = this.add.container(0, 0).setDepth(10).setVisible(false)
 
-    this._buildHub()
-    this._useMethods()
+    this.buildHub()
   }
 
-  private _buildHub() {}
-  _showHub() {}
-  _showCategory(_id: CategoryId) {}
-  _showModal(_index: number) {}
-  _closeModal() {}
-  private _destroyActiveBgObjects() {}
-  private _renderBossBg(_key: string) {}
-  private _buildCategoryPage(_id: CategoryId) {}
-  private _buildEntries(_id: CategoryId): DisplayEntry[] { return [] }
-  private _getFilters(_id: CategoryId): string[] { return [] }
-  private _buildFilterTabs(_id: CategoryId) {}
-  private _buildGrid() {}
-  private _buildModalContent(_entry: DisplayEntry, _w: number, _h: number): Phaser.GameObjects.GameObject[] { return [] }
-
-  private _useMethods() {
-    // Mark stub methods as referenced to satisfy noUnusedLocals
-    void this._destroyActiveBgObjects
-    void this._renderBossBg
-    void this._buildCategoryPage
-    void this._buildEntries
-    void this._getFilters
-    void this._buildFilterTabs
-    void this._buildGrid
-    void this._buildModalContent
-  }
+  buildHub() {}
+  showHub() {}
+  showCategory(_id: CategoryId) {}
+  showModal(_index: number) {}
+  closeModal() {}
 }
